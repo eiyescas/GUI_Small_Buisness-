f3acdb9e8893ec40f1b2810c595006b93bf74680
17862 <17862@10.0.0.212>
2019-10-15 00:13:30 -0400

    First commit. Business

---
+++ src/gui_small_buisness/GUI_Small_Buisness.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui_small_buisness;
+
+/**
+ *
+ * @author 17862
+ */
+public class GUI_Small_Buisness {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
